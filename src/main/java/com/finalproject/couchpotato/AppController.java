5ff9112f2a868a5b878f2a801964f2ff757817ae
Dev-Selina <48724379+Dev-Selina@users.noreply.github.com>
2019-04-22 22:46:38 +0100

    Update AppController.java
    "Mapping"-model added to controller for movies only, still requires users, actors and reviews models (/&mapping).

--- src/main/java/com/finalproject/couchpotato/AppController.java
+++ src/main/java/com/finalproject/couchpotato/AppController.java
@@ -3,6 +3,9 @@ package com.finalproject.couchpotato;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class AppController {
@@ -13,27 +16,68 @@ public class AppController {
     Users user = new Users();
 
     @GetMapping("/viewMovies")
-    public String viewMovies(Model model){
+    public String viewMovies(Model model) {
         movie.getAllMovies();
         model.addAttribute("movie", Movies.movies);
         return "viewMovies";
     }
 
     @GetMapping("/addMovie")
-    public String addMovie(Model model) {
+    public String movieToAdd(Model model) {
         int nextID = Movies.movies.size() + 1;
         System.out.println(nextID);
+        Movies movie = new Movies();
+        movie.setMovie_id(nextID);
+        model.addAttribute("movie", movie);
+        return "addMovie";
+    }
 
-
-    @GetMapping("/movieAdded")
+    @PostMapping("/addingMovie")
+    public String movieAdded(@ModelAttribute Movies movie) {
+        movie.addNewMovie(movie);
+        return "viewMovies";
+    }
 
     @GetMapping("/editDeleteMovie")
+    public String editDeleteMovie(Model model) {
+        model.addAttribute("movie", Movies.movies);
+        model.addAttribute("movieEdit", new Movies());
+        return "editDeleteMovie";
+    }
 
-    @GetMapping("/bookSaved")
+    @PostMapping("/editMovie")
+    public String movieToEdit(@ModelAttribute Movies movie, Model model) {
+        Movies m = new Movies();
+        for (Movies mv : Movies.movies) {
+            if (mv.getMovie_id() == movie.getMovie_id()) {
+                m = mv;
+            }
+        }
+        model.addAttribute("movie", m);
+        return "editMovie";
+    }
 
-    @GetMapping("/bookDelete")
+    @PostMapping("/movieSaved")
+    public String movieSaved(@ModelAttribute Movies movie) {
+        movie.updateMovie(movie);
+        return "viewMovies";
+    }
 
-    @GetMapping("/bookDeleted")
+    @PostMapping("/movieDelete")
+    public String movieToDelete(@ModelAttribute Movies movie, Model model) {
+        Movies m = new Movies();
+        for (Movies mv : Movies.movies) {
+            if (mv.getMovie_id() == movie.getMovie_id()) {
+                m = mv;
+            }
+        }
+        model.addAttribute("movie", m);
+        return "movieDelete";
+    }
 
+    @PostMapping("/movieDeleted")
+    public String movieDeleted(@ModelAttribute Movies movie) {
+        movie.deleteMovie(movie);
+        return "viewMovies";
+    }
 }
-
