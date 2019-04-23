0fb51a277d344c5429a7abd53bf1c3501de65a3a
Danillo Liziero <danliziero@gmail.com>
2019-04-23 16:54:31 +0100

    DB relative path corrected

--- src/main/java/com/finalproject/couchpotato/InitialiseDB.java
+++ src/main/java/com/finalproject/couchpotato/InitialiseDB.java
@@ -19,7 +19,7 @@ public class InitialiseDB {
             SQLiteConfig config = new SQLiteConfig();
             config.enforceForeignKeys(true);
             con = DriverManager.getConnection("jdbc:sqlite:" +
-                            "C:/hakuna/lib/MovieMadnessDB.db",
+                            "./lib/MovieMadnessDB.db",
                     config.toProperties());
         } catch (Exception ex) {
             System.out.println(ex.getClass());
