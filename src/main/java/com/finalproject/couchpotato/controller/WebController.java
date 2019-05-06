4f3a73ee603d83ab3ef1e4390dfd2d92fbbfcf08
Lina <42553579+pyrolina@users.noreply.github.com>
2019-05-07 00:55:09 +0100

    Customising white label page to feed back error and on-click returns to Home page

---
+++ src/main/java/com/finalproject/couchpotato/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.finalproject.couchpotato.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+    @RequestMapping(value = {"/user"})
+    public String user() {
+        return "user";
+    }
+
+    @RequestMapping(value = {"/login"})
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = {"403"})
+    public String Error403() {
+        return "403";
+    }
+}
+
