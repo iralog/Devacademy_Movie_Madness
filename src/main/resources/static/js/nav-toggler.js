4e5eca50ec12b74ea22e1549ed10e5f0cd470cee
Danillo Liziero <danliziero@gmail.com>
2019-05-09 07:50:12 +0100

    Update nav-toggler.js

--- src/main/resources/static/js/nav-toggler.js
+++ src/main/resources/static/js/nav-toggler.js
@@ -1,14 +1,14 @@
 (toggler => {
     const sideNav = document.querySelector('.side-nav-couch');
-    const hamburger = document.querySelector('#hamburger-icon-couch');
+const hamburger = document.querySelector('#hamburger-icon-couch');
 
-    hamburger.addEventListener("click", function () {
-        if (this.classList.toggle('active')) {
-            sideNav.style.left = '0';
-        } else {
-            sideNav.style.left = '-15.0rem';
-        }
-    });
+hamburger.addEventListener("click", function () {
+    if (this.classList.toggle('active')) {
+        sideNav.style.left = '0';
+    } else {
+        sideNav.style.left = '-15.6rem';
+    }
+});
 
 })();
 
